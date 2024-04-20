df1d943badb99d8d1f9bc47eac32de1f7d6488cb
phuongnd1216 <p2sw1216@gmai.com>
2024-04-20 15:45:25 +0700

    Sap xep SV

--- Bai3_Pointer/main.c
+++ Bai3_Pointer/main.c
@@ -1,39 +1,105 @@
 #include <stdio.h>
+#include <string.h>
 
-void tong(int a, int b){
-    printf("Tong cua %d va %d: %d\n", a, b, a+b);
+typedef struct {
+   char ten[50];
+   float diemTrungBinh;
+   int id;
+} SinhVien;
+
+int stringCompare(const char *str1, const char *str2) {
+   while (*str1 && (*str1 == *str2)) {
+       str1++;
+       str2++;
+   }
+   return *(const unsigned char*)str1 - *(const unsigned char*)str2;
 }
 
-void hieu(int a, int b){
-    printf("Hieu cua %d va %d: %d\n", a, b, a-b);
+
+// Hàm so sánh theo tên
+int compareByName(const void *a, const void *b) {
+   SinhVien *sv1 = (SinhVien *)a;
+   SinhVien *sv2 = (SinhVien *)b;
+   return stringCompare(sv1->ten, sv2->ten);
 }
 
-void tich(int a, int b){
-    printf("Tich cua %d va %d: %d\n", a, b, a*b);
+// Hàm so sánh theo điểm trung bình
+int compareByDiemTrungBinh(const void *a, const void *b) {
+   SinhVien *sv1 = (SinhVien *)a;
+   SinhVien *sv2 = (SinhVien *)b;
+   if (sv1->diemTrungBinh > sv2->diemTrungBinh)
+   {
+       return 1;
+   }
+  
+   return 0;
 }
 
-void thuong(int a, double b){
-    printf("Thuong cua %d va %f: %f\n", a, b, a/b);
+// Hàm so sánh theo ID
+int compareByID(const void *a, const void *b) {
+   SinhVien *sv1 = (SinhVien *)a;
+   SinhVien *sv2 = (SinhVien *)b;
+   return sv1->id - sv2->id;
 }
 
-void tinhtoan(void (*pheptoan)(int,int), int a, int b){
-    printf("Thuc hien phep toan\n");
-    pheptoan(a, b);
+// Hàm sắp xếp chung
+void sort(SinhVien array[], size_t size, int (*compareFunc)(const void *, const void *)) {
+   int i, j;
+   SinhVien temp;
+   for (i = 0; i < size-1; i++)    
+       for (j = i+1; j < size; j++)
+           if (compareFunc(array+i, array+j)>0) {
+               temp = array[i];
+               array[i] = array[j];
+               array[j] = temp;
+           }
 }
-int main (int argc, char const *argv[])
-{
-    void (*pheptoan)(int, int);
-    void (*Test)(int, double);
-    pheptoan = tong;
-    pheptoan(7,9);
-    pheptoan = hieu;
-    pheptoan(3,8);
-    pheptoan = tich;
-    pheptoan(3,5);
-    Test = thuong;
-    Test(7,3.3);
-    tinhtoan(&tong, 9, 22);
-    tinhtoan(&hieu, 1, 10);
-    tinhtoan(&tich, 2, 55);
-    return 0;
+
+void display(SinhVien *array, size_t size) {
+   for (size_t i = 0; i < size; i++) {
+       printf("ID: %d, Ten: %s, Diem Trung Binh: %.2f\n", array[i].id, array[i].ten, array[i].diemTrungBinh);
+   }
+   printf("\n");
+}
+
+int main() {
+   SinhVien danhSachSV[] = {
+       {  
+           .ten = "Hoang",
+           .diemTrungBinh = 7.5,
+           .id = 100
+       },
+       {
+           .ten = "Tuan",
+           .diemTrungBinh = 4.5,
+           .id = 101
+       },
+       {
+           .ten = "Vy",
+           .diemTrungBinh = 6.8,
+           .id = 102},
+       {  
+           .ten = "Ngan",
+           .diemTrungBinh = 5.6,
+           .id = 10
+       },
+   };
+   size_t size = sizeof(danhSachSV) / sizeof(danhSachSV[0]);
+
+   // Sắp xếp theo tên
+   sort(danhSachSV, size, compareByName);
+
+   display(danhSachSV, size);
+
+   // Sắp xếp theo điểm trung bình
+   sort(danhSachSV, size, compareByDiemTrungBinh);
+
+   display(danhSachSV, size);
+
+   // Sắp xếp theo ID
+   sort(danhSachSV, size, compareByID);
+
+   display(danhSachSV, size);
+
+   return 0;
 }
