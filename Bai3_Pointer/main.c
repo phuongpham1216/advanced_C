8a9cc4743dc7e1b8adcc10e7fe2b3dfad10cd876
phuongnd1216 <p2sw1216@gmai.com>
2024-04-20 12:45:29 +0700

    Update Con tro ham

--- Bai3_Pointer/main.c
+++ Bai3_Pointer/main.c
@@ -1,13 +1,31 @@
 #include <stdio.h>
 
+void tong(int a, int b){
+    printf("Tong cua %d va %d: %d\n", a, b, a+b);
+}
+
+void hieu(int a, int b){
+    printf("Hieu cua %d va %d: %d\n", a, b, a-b);
+}
+
+void tich(int a, int b){
+    printf("Tich cua %d va %d: %d\n", a, b, a*b);
+}
+
+void thuong(int a, double b){
+    printf("Thuong cua %d va %f: %f\n", a, b, a/b);
+}
 int main (int argc, char const *argv[])
 {
-    int var = 10;
-
-printf("Dia chi var: %p\n", &var);
-
-    int *ptr = &var;
-
-printf("Dia chi con tro ptr: %p\nGiá trị con trỏ ptr: %d\n", ptr, *ptr);
+    void (*pheptoan)(int, int);
+    void (*Test)(int, double);
+    pheptoan = &tong;
+    pheptoan(7,9);
+    pheptoan = &hieu;
+    pheptoan(3,8);
+    pheptoan = &tich;
+    pheptoan(3,5);
+    Test = &thuong;
+    Test(7,3.3);
     return 0;
 }
