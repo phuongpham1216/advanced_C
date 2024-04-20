1adbf7d761e34cd20df8f6ab112d3314254f3dc1
phuongnd1216 <p2sw1216@gmai.com>
2024-04-20 13:07:53 +0700

    Update funtion pointer

--- Bai3_Pointer/main.c
+++ Bai3_Pointer/main.c
@@ -15,17 +15,25 @@ void tich(int a, int b){
 void thuong(int a, double b){
     printf("Thuong cua %d va %f: %f\n", a, b, a/b);
 }
+
+void tinhtoan(void (*pheptoan)(int,int), int a, int b){
+    printf("Thuc hien phep toan\n");
+    pheptoan(a, b);
+}
 int main (int argc, char const *argv[])
 {
     void (*pheptoan)(int, int);
     void (*Test)(int, double);
-    pheptoan = &tong;
+    pheptoan = tong;
     pheptoan(7,9);
-    pheptoan = &hieu;
+    pheptoan = hieu;
     pheptoan(3,8);
-    pheptoan = &tich;
+    pheptoan = tich;
     pheptoan(3,5);
-    Test = &thuong;
+    Test = thuong;
     Test(7,3.3);
+    tinhtoan(&tong, 9, 22);
+    tinhtoan(&hieu, 1, 10);
+    tinhtoan(&tich, 2, 55);
     return 0;
 }
